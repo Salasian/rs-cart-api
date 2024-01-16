1932f18befb62fc7d7fdf2128067ba912e1e1607
Ian Salas <ian_salas@epam.com>
2024-01-16 13:11:32 -0700

    npx serverless offline works

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,31 @@
 import { NestFactory } from '@nestjs/core';
-
-import * as helmet from 'helmet';
-
+import serverlessExpress from '@codegenie/serverless-express';
+import { Callback, Context, Handler } from 'aws-lambda';
 import { AppModule } from './app.module';
+
+let server: Handler;
 
 const port = process.env.PORT || 4000;
 
-async function bootstrap() {
+async function bootstrap(): Promise<Handler> {
   const app = await NestFactory.create(AppModule);
+  await app.init();
 
   app.enableCors({
     origin: (req, callback) => callback(null, true),
   });
-  app.use(helmet());
 
-  await app.listen(port);
+  const expressApp = app.getHttpAdapter().getInstance();
+
+  return serverlessExpress({ app: expressApp });
 }
-bootstrap().then(() => {
+
+export const handler: Handler = async (
+  event: any,
+  context: Context,
+  callback: Callback,
+) => {
+  server = server ?? (await bootstrap());
   console.log('App is running on %s port', port);
-});
+  return server(event, context, callback);
+};
